cc23945f4a9be641f77948a622638e89cd83b5ce
Sylvain-MED <sylvain.galoustoff@gmail.com>
2025-03-30 23:51:35 +0200

    Add swipe example

--- gems/src/ActionSwipe.tsx
+++ gems/src/ActionSwipe.tsx
@@ -1,20 +1,49 @@
 import { StyleSheet, Text, View } from "react-native";
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from "react-native-reanimated";
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from "react-native-gesture-handler";
 
 type ActionSwipeProps = {
   icon?: JSX.Element;
 };
 
 function ActionnSwipe({ icon }: ActionSwipeProps): JSX.Element {
+  const offset = useSharedValue<number>(0);
+
+  const pan = Gesture.Pan()
+    .onChange((event) => {
+      offset.value = event.translationX;
+    })
+    .onFinalize(() => {
+      offset.value = withSpring(0);
+    });
+
+  const animatedStyles = useAnimatedStyle(() => ({
+    transform: [{ translateX: offset.value }],
+  }));
+
   return (
-    <View style={styles.wrapper}>
-      <View style={styles.container}>
-        <View style={styles.icon}>{icon}</View>
-        <View>
-          <Text style={styles.title}>Action swipe</Text>
-          <Text style={styles.subtitle}>Subtitle</Text>
-        </View>
+    <GestureHandlerRootView>
+      <View style={styles.wrapper}>
+        <GestureDetector gesture={pan}>
+          <Animated.View style={[styles.container, animatedStyles]}>
+            {icon && <View style={styles.icon}>{icon}</View>}
+            <View>
+              <Text style={styles.title}>Action swipe</Text>
+              <Text style={styles.subtitle}>Subtitle</Text>
+            </View>
+          </Animated.View>
+        </GestureDetector>
       </View>
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
@@ -22,10 +51,11 @@ export default ActionnSwipe;
 
 const styles = StyleSheet.create({
   wrapper: {
-    backgroundColor: "white",
     width: "100%",
+    backgroundColor: "#e6e6e6",
   },
   container: {
+    backgroundColor: "white",
     gap: 16,
     width: "100%",
     padding: 16,
